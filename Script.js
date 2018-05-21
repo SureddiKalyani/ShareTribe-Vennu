015e7dc37f5ee7f44b876e0bd00d64720cc11911
SureddKalyani <kalyani@capiot.com>
2018-05-21 10:54:32 +0530

    changes in script.

--- Script.js
+++ Script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
     setTimeout(function () {
+        $(document).ready(function () {
+            setTimeout(function () {
+        var val = $('.listing-details-container .row:eq(8)').text().trim().split(':').pop().trim();
+                var place = $('.origin')["0"].nextElementSibling.value;
+                $('.listing-details-container .row:eq(2)').html('<div class="row"><div class="col-12"> <div class="row"> <div class="col-6"><p>' + place + '</p></div>   <div class="col-6"><b>Standing / Seating</b><p>' + val + '</p> </div></div></div></div>');
+                
         
         // heading after image and hide on top
         var title = $("#listing-title").html();
