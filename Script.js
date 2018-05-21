e1cc6033f1b608ebec5acd648f128afcdde5fc92
SureddKalyani <kalyani@capiot.com>
2018-05-21 13:11:42 +0530

    reduced timeout.

--- Script.js
+++ Script.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
         var place = $('.origin')["0"].nextElementSibling.value;
         $('.listing-details-container .row:eq(2)').html('<div class="row"><div class="col-12"> <div class="row"> <div class="col-6"><p>' + place + '</p></div>   <div class="col-6"><b>Standing / Seating</b><p>' + val + '</p> </div></div></div></div>');
 
-
         // heading after image and hide on top
         var title = $("#listing-title").html();
         $(".listing-details-container div.row:first").append('<div class="row"><div class="col-12"><h1 class="heading_list">' + title + '</h1></div></div>');
@@ -16,5 +15,5 @@ $(document).ready(function () {
         var s = document.createElement("span");
         $(s).addClass("checked_color");
         $('.checkbox-container input[type=checkbox]').wrap('<span class="checked_color" />');
-    }, 5000);
+    }, 1000);
 });
