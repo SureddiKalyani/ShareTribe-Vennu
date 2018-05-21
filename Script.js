6d748c381dd2625fc18e4e7b14c4bd40e65feb35
SureddKalyani <kalyani@capiot.com>
2018-05-21 09:56:33 +0530

    removed script for testing.

--- Script.js
+++ Script.js
@@ -1,10 +1,6 @@
 $(document).ready(function () {
     setTimeout(function () {
-        // value and location after description
-        // var val = $('.listing-details-container .row:eq(8)').text().trim().split(':').pop().trim();
-        // var place = $('.origin')["0"].nextElementSibling.value;
-        // $('.listing-details-container .row:eq(2)').html('<div class="row"><div class="col-12"> <div class="row"> <div class="col-6"><p>' + place + '</p></div>   <div class="col-6"><b>Standing / Seating</b><p>' + val + '</p> </div></div></div></div>');
-
+        
         // heading after image and hide on top
         var title = $("#listing-title").html();
         $(".listing-details-container div.row:first").append('<div class="row"><div class="col-12"><h1 class="heading_list">' + title + '</h1></div></div>');
