1474261efa37b26e940f77b781bb9d447a8ce0d8
SureddKalyani <kalyani@capiot.com>
2018-05-22 11:49:37 +0530

    modified script.

--- Script.js
+++ Script.js
@@ -1,4 +1,24 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+//     console.log("Loading");
+//     setTimeout(function () {
+//         //location and value after description
+//         var val = $('.listing-details-container .row:eq(9)').text().trim().split(':').pop().trim();
+//         console.log(val);
+//         var place = $('.origin')["0"].nextElementSibling.value;
+//         $('.listing-details-container .row:eq(2)').html('<div class="row"><div class="col-12"> <div class="row"> <div class="col-6"><p>' + place + '</p></div>   <div class="col-6"><b>Standing / Seating</b><p>' + val + '</p> </div></div></div></div>');
+
+//         // heading after image and hide on top
+//         var title = $("#listing-title").html();
+//         $(".listing-details-container div.row:first").append('<div class="row"><div class="col-12"><h1 class="heading_list">' + title + '</h1></div></div>');
+//         $(".marketplace-lander").hide();
+
+//         // for checkboxes
+//         var s = document.createElement("span");
+//         $(s).addClass("checked_color");
+//         $('.checkbox-container input[type=checkbox]').wrap('<span class="checked_color" />');
+//     }, 500);
+// });
+$(window).on("load", function () {
     console.log("Loading");
     setTimeout(function () {
         //location and value after description
@@ -16,5 +36,5 @@ $(document).ready(function () {
         var s = document.createElement("span");
         $(s).addClass("checked_color");
         $('.checkbox-container input[type=checkbox]').wrap('<span class="checked_color" />');
-    }, 5000);
+    }, 500);
 });
